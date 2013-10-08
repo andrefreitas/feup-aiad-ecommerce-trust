779549e3de79af6e5f5f32d14d5f8117f74d4eb9
Ana Gomes <acgg.gomes@gmail.com>
2013-10-08 17:52:58 +0100

    Added class methods to add and remove products in Buyer's class.

--- src/ecommerce/Buyer.java
+++ src/ecommerce/Buyer.java
@@ -1,9 +1,14 @@
 package ecommerce;
 
+import java.util.ArrayList;
+
 public class Buyer extends User {
+	protected ArrayList<Order> orders;
 
 	public Buyer(String name, String country) {
 		super(name, country);
 	}
+	
+	
 
 }
